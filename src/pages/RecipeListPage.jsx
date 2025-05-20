bcd21b8aea603468b51eb6428afdb8527030f47f
Nesli <nesliparma@gmail.com>
2025-05-20 20:52:26 +0200

    building recipe list page

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,26 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 import { data } from "../utils/data";
 import { Hero } from "../components/Hero";
 import { RecipeItemCard } from "../components/RecipeItemCard";
 
-export const RecipeListPage = () => {
-  const item = data.hits[3].recipe;
+export const RecipeListPage = ({ setSelectedRecipe }) => {
+  const recipes = data.hits;
 
   return (
     <Box>
       <Hero />
-      <RecipeItemCard
-        label={item.label}
-        image={item.image}
-        dietLabels={item.dietLabels}
-        cautions={item.cautions}
-        mealType={
-          item.mealType[0].charAt(0).toUpperCase() + item.mealType[0].slice(1)
-        }
-        dishType={
-          item.dishType[0].charAt(0).toUpperCase() + item.dishType[0].slice(1)
-        }
-      />
+      <SimpleGrid minChildWidth="320px" spacing={5}>
+        {recipes.map((recipe) => (
+          <RecipeItemCard
+            recipe={recipe}
+            key={recipe.id}
+            label={recipe.label}
+            image={recipe.image}
+            dietLabels={recipe.dietLabels}
+            cautions={recipe.cautions}
+            mealType={
+              recipe.mealType[0].charAt(0).toUpperCase() +
+              recipe.mealType[0].slice(1)
+            }
+            dishType={
+              recipe.dishType[0].charAt(0).toUpperCase() +
+              recipe.dishType[0].slice(1)
+            }
+            setSelectedRecipe={setSelectedRecipe}
+          />
+        ))}
+      </SimpleGrid>
     </Box>
   );
 };

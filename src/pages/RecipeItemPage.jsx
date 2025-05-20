af10d820adfe285e87b2a0f25a65403579b1ce34
Nesli <nesliparma@gmail.com>
2025-05-20 22:45:43 +0200

    added components to recipeitempage

--- src/pages/RecipeItemPage.jsx
+++ src/pages/RecipeItemPage.jsx
@@ -1,9 +1,47 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Image,
+  Text,
+  Heading,
+  Box,
+  Button,
+  ListItem,
+  UnorderedList,
+} from "@chakra-ui/react";
 
-export const RecipeItemPage = ({ setSelectedRecipe }) => {
+export const RecipeItemPage = ({ recipe, setSelectedRecipe }) => {
   return (
-    <Button mt={5} onClick={() => setSelectedRecipe(null)}>
-      Back to recipes
-    </Button>
+    <Box>
+      <Box display="flex" flexDirection="row">
+        <Heading>
+          <Text>{recipe.label}</Text>
+          <Text fontSize={20} mt={10}>
+            For: {recipe.mealType}
+          </Text>
+          <Text fontSize={20}>Course: {recipe.dishType}</Text>
+          <Text fontSize={20}>Cooking time: {recipe.totalTime} mins</Text>
+          <Text fontSize={20}>Servings: {recipe.yield}</Text>
+        </Heading>
+        <Image
+          src={recipe.image}
+          alt={recipe.label}
+          maxWidth="500px"
+          objectFit="cover"
+        ></Image>
+      </Box>
+      <Box>
+        <Text fontWeight={800} ml={3}>
+          Ingredients
+        </Text>
+        <UnorderedList styleType="none" ml={0}>
+          {recipe.ingredientLines.map((ingredient, index) => (
+            <ListItem key={index}>- {ingredient}</ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+
+      <Button mt={5} onClick={() => setSelectedRecipe(null)}>
+        Back to recipes
+      </Button>
+    </Box>
   );
 };
